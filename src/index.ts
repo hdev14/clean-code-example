ce935bc24fee078022440235c2b33e0d5dbc23aa
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-02-06 13:58:58 -0300

    improvements

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
 dotenv.config();
-
 import router from './router';
 import getCon from './getCon';
 
-
-
 (async () => {
   let con = getCon();
-
   try {
     const app = express();
-
     app.use(express.json());
-
     app.use(router);
-
     app.listen(3000, () => {
       console.log('Server is running.');
     });
